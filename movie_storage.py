6163313999ac323f308c4251e26ad1d0d7757153
Robert Maxwell <robinix585@gmil.com>
2023-04-27 17:09:49 -0600

    Added coding for generating webpages

--- movie_storage.py
+++ movie_storage.py
@@ -1,4 +1,9 @@
 import json
+import requests
+
+JSON_FILE = "OMDB_movies.json"
+API_KEY = "d4ba49c2"
+URL = "http://www.omdbapi.com/?apikey=d4ba49c2&t="
 
 
 def list_movies():
@@ -20,7 +25,7 @@ def list_movies():
       },
     }
     """
-    with open("movies.json", "r") as jfile:
+    with open(JSON_FILE, "r") as jfile:
         movies_lst = json.load(jfile)
         return movies_lst
 
@@ -28,23 +33,38 @@ def list_movies():
 def save_db(movies):
     saved = 0
     moviesdb = json.dumps(movies)
-    with open("movies.json", "w") as jfile:
+    with open(JSON_FILE, "w") as jfile:
         jfile.write(moviesdb)
         saved = 1
     return saved
 
 
-def add_movie(title, year, rating):
+def add_movie(title):
     """
     Adds a movie to the movies database.
     Loads the information from the JSON file, add the movie,
     and saves it. The function doesn't need to validate the input.
     """
     movies = list_movies()
+    response = requests.get(URL+title)
+    try:
+        response = requests.get(URL+title)
+        response.raise_for_status()  # Raises an HTTPError for 4xx and 5xx status codes
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Something went wrong:", err)
+    if response.status_code == requests.codes.ok:
+        data = response.json()
     movies.append({
-        'Title': title,
-        'Rating': rating,
-        'Year': year
+        'Title': data['Title'],
+        'Rating': data['imdbRating'],
+        'Year': data['Year'],
+        'Poster': data['Poster']
     })
     save_db(movies)
     print(f"Movie {title} successfully added")
@@ -59,9 +79,9 @@ def delete_movie(title):
     """
     movies = list_movies()
     s = 0
-    for i in range(len(movies)):
-        if movies[i]['Title'] == title:
-            del movies[i]
+    for movie in movies:
+        if movie['Title'] == title:
+            movies.remove(movie)
             s = save_db(movies)
     if s == 1:
         print(f"Movie {title} was successfully deleted.")
--- movies.py
+++ movies.py
@@ -1,10 +1,46 @@
 import random
+import codecs
 import movie_storage as ms
+
+FILE_HTML = "_static\index_template.html"
+MOVIE_HTML = "_static\movies.html"
+
+
+def read_html(html_file):
+    """Reads a html file returns the data as a string."""
+    with open(html_file, "r") as file:
+        data = file.read()
+        return data
+
+
+def serialize_movie(movie_dict):
+    """Provides template for serializing a movie object"""
+    output = ''
+    output += '<li>\n<div class="movie">\n<img class="movie-poster"\n'
+    output += f'src={movie_dict["Poster"]}\ntitle=""/>\n'
+    output += f'<div class="movie-title">{movie_dict["Title"]}</div>\n'
+    output += f'<div class="movie-year">{movie_dict["Year"]}</div>\n'
+    output += f'<div class="movie-rating">{movie_dict["Rating"]}</div>\n'
+    output += '</div>\n</li>\n'
+    return output
+
+
+def display_movies():
+    """Creates a string containing the title, rating,
+     release year and poster
+    derived from the api."""
+    movies = ms.list_movies()
+    # define an empty string
+    output = ""
+    # serialize animal objects
+    for movie in movies:
+        output += serialize_movie(movie)
+    return output
 
 
 def my_moviesDB():
-    # Application's control Interface that allows user
-    # selection of operations to run
+    """Application's control Interface that allows user
+      selection of operations to run"""
 
     selections = """
   ********** My Movies Database **********
@@ -51,22 +87,20 @@ def add_movie():
             print(
                 f"Error: The movie {new_movie} already exists in the database.")
             add_movie()
-    rating = float(input("Enter new movie rating (1-10): "))
-    release_year = float(input("Enter the movie's release year: "))
-    ms.add_movie(new_movie, release_year, rating)
+    ms.add_movie(new_movie)
     con = input("""Press enter to continue: """)
     my_moviesDB()
 
 
 def del_movie():
-    """ Deletes a movie from the database """
+    """Deletes a movie from the database"""
     title = input("Enter movie name to delete: ")
     ms.delete_movie(title)
     return_to_menu()
 
 
 def update_movie():
-    # Updates the movie's rating
+    """Updates the movie's rating"""
     movies = ms.list_movies()
     title = input("Enter movie name: ")
     # checks if movie exists in dictionary
@@ -78,14 +112,14 @@ def update_movie():
 
 
 def return_to_menu():
-    # Returns the app to the menu of operations
+    """Returns the app to the menu of operations"""
     print(" ")
     con = input("Press enter to continue: ")
     my_moviesDB()
 
 
 def check_stats():
-    # Displays movie stats based on the ratings
+    """Displays movie stats based on the ratings"""
     movies_lst = ms.list_movies()
     avg_rating = 0
     sum_rating = 0
@@ -95,8 +129,8 @@ def check_stats():
 
     # calculates the average rating
     for movie in movies_lst:
-        movie_ratings.append(movie['Rating'])
-        sum_rating += movie['Rating']
+        movie_ratings.append(float(movie['Rating']))
+        sum_rating += float(movie['Rating'])
     avg_rating = sum_rating / len(movies_lst)
 
     sorted_val = sorted(movie_ratings)
@@ -117,9 +151,9 @@ def check_stats():
     best = max(movie_ratings)
     worst = min(movie_ratings)
     for movie in movies_lst:
-        if movie['Rating'] == best:
+        if float(movie['Rating']) == best:
             best_movie = movie['Title']
-        if movie['Rating'] == worst:
+        if float(movie['Rating']) == worst:
             worst_movie = movie['Title']
 
     # displays summary of stats
@@ -131,7 +165,7 @@ def check_stats():
 
 
 def random_movie():
-    # selects a random movie from list of dictionary movies
+    """selects a random movie from list of movies' dictionaries"""
     movies_lst = ms.list_movies()
     rand_movie = random.choice(movies_lst)
     print(
@@ -140,7 +174,7 @@ def random_movie():
 
 
 def search_movie():
-    # searches for movie titles using full title or substrings
+    """searches for movie titles using full title or substrings"""
     movies_lst = ms.list_movies()
     movie_name = input("Enter part of movie name: ").lower()
     for i in range(len(movies_lst)):
@@ -152,29 +186,40 @@ def search_movie():
 
 
 def sort_by_rating():
-    # sorts movie titles based on ratings in descending order
+    """Sorts movie titles based on ratings in descending order"""
     movies_lst = ms.list_movies()
     movie_ratings = []
     for movie in movies_lst:
-        movie_ratings.append(movie['Rating'])
+        movie_ratings.append(float(movie['Rating']))
     sorted_lst = sorted(movie_ratings, key=lambda x: x, reverse=True)
     for rating in sorted_lst:
         for movie in movies_lst:
-            if movie['Rating'] == rating:
+            if float(movie['Rating']) == rating:
                 print(f"{movie['Title']}: {movie['Rating']}")
     return_to_menu()
 
 
 def exit_movies():
+    """Exits the application"""
     stop = 1
     print("Bye!")
     return stop
 
 
 def generate_website():
+    """Generates website from list of movies in the database."""
+    html_data = read_html(FILE_HTML)
+    # replace text placeholder with json data
+    html_data = html_data.replace("__TEMPLATE_TITLE__", "My OMDB Movie App")
+    html_data = html_data.replace("__TEMPLATE_MOVIE_GRID__", display_movies())
+    # write html_data to a new file
+    with codecs.open(MOVIE_HTML, "w", encoding="utf-8") as file:
+        file.write(html_data)
+    print("Website was generated successfully.")
     return_to_menu()
 
 
+"""Dispatcher dictionary for running all functions"""
 choice_dict = {
     "0": exit_movies,
     "1": list_movies,
